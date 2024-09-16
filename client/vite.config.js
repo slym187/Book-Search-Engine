3d15cf1924b6d7faaf26e03ce126642ccb820036
Ruby Hill <163611320+slym187@users.noreply.github.com>
2024-09-16 09:54:27 -0500

    build again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Change output directory to 'build'
+    outDir: 'build', // Output directory for build files
   },
   server: {
-    port: 3000,
-    open: true,
+    port: 3000, // The development server will run on port 3000
+    open: true, // Automatically open the browser on server start
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3001', // Proxy API requests to the backend server
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
+
