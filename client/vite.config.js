d6d24e4b92aa4ebd63dd27fbdb137ab892dc278f
Ruby Hill <163611320+slym187@users.noreply.github.com>
2024-09-16 01:38:30 -0500

    Configure Vite build output and update server to serve static files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change output directory to 'build'
+  },
   server: {
     port: 3000,
     open: true,
